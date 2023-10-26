b3b16bd479ecd8ca1089c1dc3c34a535ccf36c57
F8S-is-me <johnasgarnica@gmail.com>
2023-10-26 15:15:00 -0700

    added rust

---
+++ backend/rusthome/target/debug/deps/rusthome-ae15b249dacff44b.d
@@ -0,0 +1,5 @@
+/Users/johnny/Desktop/projects/FirstProj/backend/rusthome/target/debug/deps/rusthome-ae15b249dacff44b.rmeta: src/main.rs
+
+/Users/johnny/Desktop/projects/FirstProj/backend/rusthome/target/debug/deps/rusthome-ae15b249dacff44b.d: src/main.rs
+
+src/main.rs:
---
+++ backend/rusthome/target/debug/rusthome.d
@@ -0,0 +1,1 @@
+/Users/johnny/Desktop/projects/FirstProj/backend/rusthome/target/debug/rusthome: /Users/johnny/Desktop/projects/FirstProj/backend/rusthome/src/main.rs
